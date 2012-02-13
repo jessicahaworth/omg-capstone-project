d9bfd546b54f8d8a650413aaf49e863fc2d83407
David Piskac <dpiskac@gmail.com>
2012-02-13 16:29:18 -0600

    pushing updated datasources.groovy, again

--- capstone_project/grails-app/conf/DataSource.groovy
+++ capstone_project/grails-app/conf/DataSource.groovy
@@ -41,3 +41,40 @@ environments {
         }
     }
 }
+/*
+dataSource {
+	pooled = true
+	driverClassName = "com.mysql.jdbc.Driver"
+	username = "grails"
+	password = "server"
+}
+hibernate {
+	cache.use_second_level_cache = true
+	cache.use_query_cache = true
+	cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
+}
+
+// environment specific settings
+environments {
+	development {
+		dataSource {
+			// one of 'create', 'create-drop','update'
+			dbCreate = "create-drop"
+			// NOTE: the JDBC connection string should be
+			// all on the same line.
+			url = "jdbc:mysql://localhost:3306/racetrack_dev?autoreconnect=true"
+		}
+	}
+	test {
+		dataSource {
+			dbCreate = "update"
+			url = "jdbc:mysql://localhost:3306/racetrack_dev?autoreconnect=true"
+		}
+	}
+	production {
+		dataSource {
+			dbCreate = "update"
+			url = "jdbc:mysql://localhost:3306/racetrack_dev?autoreconnect=true"
+		}
+	}
+}*/
