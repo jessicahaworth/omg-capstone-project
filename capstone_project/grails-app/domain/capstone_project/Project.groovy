a7b85132a536313370967120ce8cdee9f8413075
David Piskac <dpiskac@gmail.com>
2012-03-10 21:36:00 -0600

    working on projects

--- capstone_project/grails-app/domain/capstone_project/Project.groovy
+++ capstone_project/grails-app/domain/capstone_project/Project.groovy
@@ -1,7 +1,21 @@
 package capstone_project
+import org.grails.comments.*
+import java.util.Date;
+import org.apache.commons.collections.list.LazyList;
+import org.apache.commons.collections.FactoryUtils;
+import org.grails.comments.Commentable;
 
-class Project {
-
+class Project implements Commentable 
+{
+	long owner_id
+	String name
+	
+	Date dateCreated
+	Date lastUpdated
+	
+	List Users = new ArrayList()
+	static hasMany = [Users:User]
+	
     static constraints = {
 		String name
     }
